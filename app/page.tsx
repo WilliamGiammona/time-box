9138805f7f9d0f0600c4d79d9e6839db4b33253a
Manish Manwani <manishmanwani73@gmail.com>
2024-02-09 17:27:41 +1030

    added eslint and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
     return (
@@ -13,8 +13,9 @@ export default function Home() {
                         className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
                         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
                         target="_blank"
-                        rel="noopener noreferrer">
-                        By{" "}
+                        rel="noopener noreferrer"
+                    >
+                        By{' '}
                         <Image
                             src="/vercel.svg"
                             alt="Vercel Logo"
@@ -43,9 +44,10 @@ export default function Home() {
                     href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
                     className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                     target="_blank"
-                    rel="noopener noreferrer">
+                    rel="noopener noreferrer"
+                >
                     <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Docs{" "}
+                        Docs{' '}
                         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                             -&gt;
                         </span>
@@ -60,9 +62,10 @@ export default function Home() {
                     href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
                     className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                     target="_blank"
-                    rel="noopener noreferrer">
+                    rel="noopener noreferrer"
+                >
                     <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Learn{" "}
+                        Learn{' '}
                         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                             -&gt;
                         </span>
@@ -77,9 +80,10 @@ export default function Home() {
                     href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
                     className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                     target="_blank"
-                    rel="noopener noreferrer">
+                    rel="noopener noreferrer"
+                >
                     <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Templates{" "}
+                        Templates{' '}
                         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                             -&gt;
                         </span>
@@ -93,15 +97,17 @@ export default function Home() {
                     href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
                     className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                     target="_blank"
-                    rel="noopener noreferrer">
+                    rel="noopener noreferrer"
+                >
                     <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Deploy{" "}
+                        Deploy{' '}
                         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                             -&gt;
                         </span>
                     </h2>
                     <p
-                        className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
+                        className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}
+                    >
                         Instantly deploy your Next.js site to a shareable URL
                         with Vercel.
                     </p>
